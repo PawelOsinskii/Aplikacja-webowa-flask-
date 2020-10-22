15594842287909a45708a3f6d8d260c5f299d599
Paweł Osiński <pawel.osinski@pentacomp.pl>
2020-10-22 09:39:58 +0200

    elo

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 app.debug = False
 
+users = {'posinski': {'firstname': 'Pawel', 'lastname': 'Osinski'
+                      }
+         }
+
 
 @app.route('/check/<username>')
 def check(username):
-    if username == 'posinski':
+    if username in users:
         return {username: 'taken'}
     return {username: 'available'}
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
