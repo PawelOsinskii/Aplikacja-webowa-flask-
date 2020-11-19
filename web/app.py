42233ce243c03a3a927c0da83c37aa6196470ece
PawelOsinski <paawel97@gmail.com>
2020-11-19 16:45:06 +0100

    drugiKamien

--- web/app.py
+++ web/app.py
@@ -1,19 +1,26 @@
+import uuid
+from datetime import timedelta
+import datetime
 from flask import Flask, render_template, redirect, jsonify, g
 from flask import request, make_response, session
 from flask import flash, url_for
 from flask_session import Session
-
 from redis import Redis
-
-db = Redis(host='redis', port=6379, db=0)
 
 from os import getenv
 from dotenv import load_dotenv
 
+# from jwt import encode, decode
+
 load_dotenv()  # zaczytuje .env
+REDIS_LOKAL = getenv('REDIS_LOKAL')
+db = Redis(host=REDIS_LOKAL, port=6379, db=0)  # wczytywać połączenie z env
 SESSION_TYPE = 'redis'  # trzymanie danych sesyjnych w redisie
 SESSION_REDIS = db  # obiekt reprezentujacy połączene
-
+SESSION_COOKIE_SECURE = True
+REMEMBER_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+REMEMBER_COOKIE_HTTPONLY = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.secret_key = getenv('SECRET_KEY')
@@ -21,6 +28,17 @@ ses = Session(app)
 app.debug = False
 
 from bcrypt import hashpw, gensalt, checkpw
+
+
+# def generate_tracking_token():
+#   payload = {}
+#  token = encode(payload,)
+# return token
+
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=5)
 
 
 def is_user(username):
@@ -134,7 +152,7 @@ def is_available(user):
 def login():
     username = request.form.get('login')
     password = request.form.get('password')
-    app.logger.warning(username+'+'+password)
+    app.logger.warning(username + '+' + password)
     if not username and not password:
         flash("You need fill in username AND password")
         return redirect(url_for('login_form'))
@@ -142,18 +160,72 @@ def login():
         flash("Wrong password or login")
         return redirect(url_for('login_form'))
 
-    flash(f"Welcome in our website, now you can send the parcel. Do you know our offer?")
-    session["username"] = username
-    import datetime
-    session['logged-at'] = datetime.datetime.now()
+    flash(f"Welcome {username} in our website, now you can send the parcel. Do you know our offer?")
+    session["username"] = True
+
+    now = datetime.datetime.now()
+    session["USERNAME"] = username
+    session['logged-at'] = now.strftime("%m/%d/%Y, %H:%M:%S")
     return redirect(url_for('login_form'))
 
 
 @app.route('/sender/logout', methods=['GET'])
 def logout():
+    if "username" not in session:
+        flash("the first you need log on")
+        return redirect(url_for("login_form"))
     session.clear()
     flash("Logout success")
     return redirect(url_for('login_form'))
+
+
+@app.route('/sender/dashboard', methods=['GET'])
+def dashboard():
+    if "username" not in session:
+        flash("the first you need log on")
+        return redirect(url_for("login_form"))
+    flash(f'Hello {session["USERNAME"]} here you can apply for sending package. ')
+    return render_template("sender/dashboard.html")
+
+
+@app.route('/sender/createpackage', methods=['POST'])
+def create_package():
+    addressee = request.form.get('addressee')
+    if not addressee:
+        flash("No addressee provided")
+
+    id_postbox = request.form.get('id-postbox')
+    if not id_postbox:
+        flash("No id-postbox provided")
+
+    size = request.form.get('size')
+    if not size:
+        flash("No size provided")
+    now = datetime.datetime.now()
+
+    date = now.strftime("%m/%d/%Y, %H:%M:%S")
+
+    uid = str(uuid.uuid4())
+    if "username" not in session:
+        flash("the first you need log on")
+        return redirect(url_for("dashboard"))
+    username = session["USERNAME"]
+
+    if addressee and id_postbox and size:
+        success = save_package(addressee, id_postbox, size, date, uid, username)
+        if not success:
+            flash("error in during saving, please try again")
+            return redirect(url_for("dashboard"))
+    else:
+        return redirect(url_for("dashboard"))
+    flash("the package has been shipped")
+    return redirect(url_for("dashboard"))
+
+
+def save_package(addressee, id_postbox, size, date, uid, username):
+    db.hset(f"package:{username}", uid,
+            (addressee + '|' + id_postbox + "|" + size + "|" + date + "|"))
+    return True
 
 
 if __name__ == '__main__':
